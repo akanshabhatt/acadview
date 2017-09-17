368313f9c370d1c7768a73a89b4e0903ac989f43
akanshabhatt <akanshabhatt06d@gmail.com>
2017-09-17 18:32:30 +0530

    welcome_spy

---
+++ main.py
@@ -0,0 +1,5 @@
+spy_name=raw_input("Welcome to SpyChat, please tell your spyname first :")
+print "Welcome "+spy_name+" Glad to have you back."
+spy_salutation=raw_input("Should I call you Mr. or Ms.?")
+spy_name=spy_salutation+" "+spy_name
+print "Alright "+spy_name+" I'd like to know a little bit more about you."
