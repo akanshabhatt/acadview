031c565086f37c7f10b9f8d9b24b2b06e5259504
akanshabhatt <akanshabhatt06d@gmail.com>
2017-09-25 18:59:06 +0530

    updated spy_details

--- spydetails.py
+++ spydetails.py
@@ -1,5 +1,7 @@
-spy_name="Akansha Bhatt"
-spy_salutation="Ms."
-spy_age=20
-spy_rating=4.5
-spy_online_status=True
+spy={
+    'spy_name' : 'Akansha Bhatt',
+    'spy_salutation' : 'Ms.',
+    'spy_age' : 20,
+    'spy_rating' : 4.5,
+    'spy_online_status' : True
+}
